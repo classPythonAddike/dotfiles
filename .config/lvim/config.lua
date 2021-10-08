d8eedeba93b2bb969afed053ee2b7189eea2f1f4
classPythonAddike <classpythonaddike@gmail.com>
2021-10-08 16:00:15 +0530

    Lvim config

---
+++ .config/lvim/config.lua
@@ -0,0 +1,32 @@
+-- general
+lvim.format_on_save = true
+lvim.lint_on_save = true
+lvim.colorscheme = "nord"
+
+-- keymappings [view all the defaults by pressing <leader>Lk]
+lvim.leader = "space"
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+
+-- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
+lvim.builtin.dashboard.active = true
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.show_icons.git = 0
+
+-- if you don't want all the parsers change this to a table of the ones you want
+lvim.builtin.treesitter.ensure_installed = {}
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enabled = true
+
+-- Additional Plugins
+lvim.plugins = {
+  {
+    'norcalli/nvim-colorizer.lua',
+    config = function ()
+      require "colorizer".setup()
+    end
+  },
+  {
+    'shaunsingh/nord.nvim'
+  }
+}
